6f4510ce05892a8b646250d9a05d3157867c809d
Nevan Samadhana <nsamadhana@gmail.com>
2023-04-27 18:23:11 -0700

    Added sign in buttons/authentication is so broken

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -5,8 +5,6 @@ import {auth} from '../../utils/firebase';
 export default function Login() {
     //Sign in with Google 
     const googleProvider = new GoogleAuthProvider();
-
-    //Asynch because it might take time to login 
     const GoogleLogin = async () => {
         try {
             const result = await signInWithPopup(auth,googleProvider)
@@ -15,16 +13,15 @@ export default function Login() {
         }
 
     }
-
-
     return(
         <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
             <h2 className="text-2xl font-medium">Join Today</h2>
             <div className="py-3">
                 <h3 className="py-4 font-medium">Sign in with one of the providers</h3>
-                <button className="text-white bg-gray-700 font-medium rounded-lg p-4 flex align-middle gap-2">
+                <button 
+                onClick = {GoogleLogin}
+                className="text-white bg-gray-700 font-medium rounded-lg p-4 flex align-middle gap-2">
                 <FcGoogle className='text-2xl'/>Sign in with Google
-                
                 </button>
             </div>
         </div>
